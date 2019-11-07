34c5be8fb61d7e8971f3ccc3f1b21193ec867f7f
Roshan Gurung <Roshan.Gurung_005@outlook.com>
2019-11-07 15:28:28 -0800

    Add project files.

---
+++ ArrayL/ArrayList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new[] { 2, 4, 5, 7, 8 };
+            Console.WriteLine(numbers.Length);
+
+
+            var index = Array.IndexOf(numbers, 5);
+            Console.WriteLine(index);
+            Console.ReadLine();
+        }
+    }
+}
